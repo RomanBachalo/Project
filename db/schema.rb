96c443c412e568d1d928fd6ad56c8e8c4db863ba
RomanBachalo <romanbachalos@gmail.com>
2020-05-26 22:07:08 +0300

    fix commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200526113348) do
+ActiveRecord::Schema.define(version: 20200526172151) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "post_id"
+    t.integer "user_id"
   end
 
   create_table "feedbacks", force: :cascade do |t|
